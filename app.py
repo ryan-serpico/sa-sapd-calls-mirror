414be1364b061360175b3211639420db4b8c961e
Ryan Serpico <Ryguy360@gmail.com>
2022-10-27 11:33:54 -0500

    Bot can now analyze data for ZIP code area trends.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import json
 import os
+from datetime import datetime
 
 import pandas as pd
 import requests
@@ -101,5 +101,95 @@ def breakup_archive(data):
         for year in df['Year'].unique():
             df[(df['Month'] == month) & (df['Year'] == year)].to_csv(f'output/{year}-{month}.csv', index=False)
 
+def population_data_import():
+    population_df = pd.read_csv('input/ACSDT5Y2020.B01003-Data.csv', skiprows=1)
+
+    # Drop the first column by index
+    population_df = population_df.drop(population_df.columns[0], axis=1)
+
+    # Drop the last three columns by index
+    population_df = population_df.drop(population_df.columns[-4:], axis=1)
+
+    # Rename the columns
+    population_df.columns = ['Zipcode', 'Population']
+
+    # Remove "ZCTA5 " from the Zipcode column
+    population_df['Zipcode'] = population_df['Zipcode'].str.replace('ZCTA5 ', '')
+
+    return population_df
+
+def data_analysis():
+    '''
+    This function performs some basic analysis on the data.
+    '''
+    print('👉 Performing data analysis ...')
+
+    population_df = population_data_import()
+
+    # Loop through every csv in the output directory, ignore folders
+    for file in os.listdir('output'):
+        if os.path.isfile(os.path.join('output', file)):
+            if file == '.DS_Store':
+                continue
+
+            # Go through the "Date" column in each file to check if it contains the last date of the month
+            df = pd.read_csv(f'output/{file}')
+
+            # Extract the YY and MM from the filename
+            year = file.split('-')[0]
+            month = int(file.split('-')[1].split('.')[0])
+
+            # Check to see if the analysis file already exists in the analysis directory. If it does, skip it. If it doesn't, create it. This is to prevent the script from overwriting the analysis file each time it runs. The analysis file will only be updated once a month.
+            if not os.path.exists(f'output/analysis/{year}-{month}-analysis.csv'):
+                print(f'👉 {year}-{month}-analysis.csv does not exist!')
+            else:
+                print(f'👉 {year}-{month}-analysis.csv already exists! Skipping ...')
+                continue
+
+            # Get the current MM and YY
+            current_month = int(datetime.now().month)
+            df['Date'] = pd.to_datetime(df['Date'])
+            df['LastDayOfMonth'] = df['Date'].dt.is_month_end
+
+            if month + 1 == current_month and df['LastDayOfMonth'].any():
+                print(f'👉 Analyzing {file} ...')
+
+                # If the data in the zipcode column has more than 5 digits, remove the hyphen and everything after it
+                df['Zipcode'] = df['Zipcode'].str.split('-', expand=True)[0]
+
+                # Count how many times each ZIP code appears in the "Zipcode" column
+                zip_counts_df = df['Zipcode'].value_counts().to_frame().reset_index().rename(columns={'index': 'Zipcode', 'Zipcode': 'Count'})
+
+                # Merge the zip_counts_df dataframe with the population_df dataframe
+                zip_counts_df = zip_counts_df.merge(population_df, on='Zipcode', how='left')
+
+                # Reorder the columns
+                zip_counts_df = zip_counts_df[['Zipcode', 'Population', 'Count']]
+
+                # Calculate the incidents per 1,000 people and round to one decimal place
+                zip_counts_df['IncidentsPer1000'] = round(zip_counts_df['Count'] / zip_counts_df['Population'] * 1000, 1)
+                
+                # Sort the dataframe by the "Count" column
+                zip_counts_df = zip_counts_df.sort_values(by='IncidentsPer1000', ascending=False)
+
+                # Remove any rows where the "IncidentsPer1000" column is NaN
+                zip_counts_df = zip_counts_df.dropna(subset=['IncidentsPer1000'])
+
+                # Find the most common "ProblemType" for each ZIP code and the number of times it appears (NOTE: Review how copilot is doing this. You verified that it's accurate on Thursday, October 27, 2022.)
+                zip_problem_df = df.groupby(['Zipcode', 'ProblemType']).size().to_frame('Count').reset_index().sort_values(by=['Zipcode', 'Count'], ascending=False).groupby('Zipcode').head(1)
+
+                # Merge the zip_problem_df dataframe with the zip_counts_df dataframe
+                zip_analysis = zip_counts_df.merge(zip_problem_df, on='Zipcode', how='left')
+
+                # Rename "Count_x" to "Count" and "Count_y" to "ProblemCount"
+                zip_analysis = zip_analysis.rename(columns={'Count_x': 'Count', 'Count_y': 'ProblemCount'})
+
+                zip_analysis.to_csv(f'output/analysis/{year}-{month}-analysis.csv', index=False)
+
+                print(f'👉 {year}-{month}-analysis.csv created!')
+
+
 compile_data()
+
+data_analysis()
 
